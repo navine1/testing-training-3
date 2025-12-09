8ebaafc26de00adc72275cf1880436c7345fe37f
navine1 <thanavine.lecocq@eduvaud.ch>
2025-12-09 14:35:54 +0100

    feat: all getters and setters

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -8,49 +8,69 @@ module.exports = class CartItem {
 
     //region private attributes
     //TODO Missing private attributs
+    #ArticleId;
+    #Name;
+    #quantity;
+    #price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
         //TODO Implement this method
+        this.#articleId = articleId;
+        this.#name = name;
+        this.quantity = quantity;
+        this.price = price;
     }
 
     get articleId() {
         //TODO Implement this method
+        return this.#ArticleId;
     }
 
     get name() {
         //TODO Implement this method
+        return this.#Name;
     }
 
     get quantity() {
         //TODO Implement this method
+        return this.#quantity;
     }
 
     set quantity(value) {
         //TODO Implement this method
+        this.#validateQuantity(value);
+        this.#quantity = value;
     }
 
     get price() {
         //TODO Implement this method
+        return this.#price;
     }
 
     set price(value) {
         //TODO Implement this method
+        this.#validatePrice(value);
+        this.#price = value;
     }
 
     get total() {
         //TODO Implement this method
+        return this.#quantity * this.#price;
     }
     //endregion public methods
 
     //region private methods
     set #articleId(value) {
         //TODO Implement this method
+        this.#validateArticleId(value);
+        this.#ArticleId = value;
     }
 
     set #name(value) {
         //TODO Implement this method
+        this.#Name = value;
     }
 
     #validateArticleId(articleId) {
